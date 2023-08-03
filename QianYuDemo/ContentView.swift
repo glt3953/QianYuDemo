fdcbf0baadf64837388d1a92c14b1126ee13c193
glt3953 <395313544@qq.com>
2023-08-03 10:48:33 +0800

    初始化工程

---
+++ QianYuDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QianYuDemo
+//
+//  Created by NingXia on 2023/8/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QianYuDemo/QianYuDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QianYuDemoApp.swift
+//  QianYuDemo
+//
+//  Created by NingXia on 2023/8/3.
+//
+
+import SwiftUI
+
+@main
+struct QianYuDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
